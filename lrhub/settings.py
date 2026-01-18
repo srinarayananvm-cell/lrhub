500e1eefa00c385ef4f76620756a9f2204e6310a
Srinarayanan <srinarayananvm@gmail.com>
2026-01-18 21:07:27 +0530

    Update project files and .gitignore

--- lrhub/settings.py
+++ lrhub/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'lrhub.wsgi.application'
 # --- Database ---
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arivuverse_db',
+        'USER': 'root',
+        'PASSWORD': 'ArivuVerse@2026',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 # --- Password validation ---
 AUTH_PASSWORD_VALIDATORS = [
